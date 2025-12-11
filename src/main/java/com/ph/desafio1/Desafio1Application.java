d2314904a778ce383db763649eaba02e69df4a4e
phLinux <paulohtolotti@alumni.usp.br>
2025-12-10 21:07:19 -0300

    feat: classe principal do Spring com estrutura para aplicação no console

--- src/main/java/com/ph/desafio1/Desafio1Application.java
+++ src/main/java/com/ph/desafio1/Desafio1Application.java
@@ -1,13 +1,50 @@
 package com.ph.desafio1;
 
+import com.ph.desafio1.entities.Order;
+import com.ph.desafio1.services.OrderService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class Desafio1Application {
+import java.util.Scanner;
 
+@SpringBootApplication
+public class Desafio1Application implements CommandLineRunner {
+
+    private OrderService orderService;
+
+    public Desafio1Application(OrderService orderService) {
+        this.orderService = orderService;
+    }
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1Application.class, args);
 	}
 
+    @Override
+    public void run(String... args) throws Exception {
+
+        Scanner sc = new Scanner(System.in);
+
+        try {
+
+            System.out.print("Order Id: ");
+            int id = sc.nextInt();
+            System.out.print("Full Product value: ");
+            double basic = sc.nextDouble();
+            System.out.print("Discount value (in %): ");
+            double discount = sc.nextDouble();
+
+            Order order = new Order(id, basic, discount);
+            System.out.println("==============================");
+            System.out.println("Order Id: " + order.getCode());
+            System.out.printf("Total value: R$%.2f", this.orderService.total(order));
+
+        } catch(RuntimeException err) {
+            System.out.println(err.getMessage());
+
+        } finally {
+            sc.close();
+        }
+
+    }
 }
