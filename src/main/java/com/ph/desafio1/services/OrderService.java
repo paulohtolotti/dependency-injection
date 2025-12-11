b0a5aa26490b0af48ddc85a6b2103bfa8d94ed5a
phLinux <paulohtolotti@alumni.usp.br>
2025-12-10 21:06:53 -0300

    feat: classe service para preço de pedidos

--- src/main/java/com/ph/desafio1/services/OrderService.java
+++ src/main/java/com/ph/desafio1/services/OrderService.java
@@ -1,4 +1,19 @@
 package com.ph.desafio1.services;
 
+import com.ph.desafio1.entities.Order;
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
+
+    private ShippingService shippingService;
+
+    public OrderService(ShippingService shippingService) {
+        this.shippingService = shippingService;
+    }
+
+    public double total(Order order) {
+        double discount = 1 - order.getDiscount() / 100;
+        return order.getBasic() * discount + this.shippingService.shipment(order);
+    }
 }
